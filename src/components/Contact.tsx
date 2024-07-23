7211b639efb337a4f245bb80e66c7696f4608870
Vihang Agarwal <vihang@vihangs-mbp.lan>
2024-07-22 22:13:30 -0700

    dark mode edits

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -49,41 +49,41 @@ const ContactUs = () => {
             <div className="py-4 lg:py-8 px-4 mx-auto">
                 <form onSubmit={handleSubmit} className="space-y-8">
                     <div>
-                        <label htmlFor='email' className="block mb-2 font-medium text-gray-900 dark:text-gray-300">Email Address</label>
+                        <label htmlFor='email' className="block mb-2 font-medium text-gray-900">Email Address</label>
                         <input 
                             type="email" 
                             id="email"
                             name="email"
                             onChange={(event) => handleInputChange(event)}
-                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" 
                             placeholder="support@aifithub.com" 
                             required>    
                         </input>
                     </div>
                     <div>
-                        <label htmlFor='name' className="block mb-2 font-medium text-gray-900 dark:text-gray-300">Your Name</label>
+                        <label htmlFor='name' className="block mb-2 font-medium text-gray-900">Your Name</label>
                         <input 
                             type="text" 
                             id="name"
                             name="name"
                             onChange={(event) => handleInputChange(event)}
-                            className="block p-3 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                            className="block p-3 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" 
                             placeholder="First name" 
                             required>
                         </input>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor='message' className="block mb-2 font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                        <label htmlFor='message' className="block mb-2 font-medium text-gray-900">Your message</label>
                         <textarea 
                             id="message" 
                             rows={6}
                             name="message"
                             onChange={(event) => handleInputChange(event)}
-                            className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                            className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" 
                             placeholder="What do you want to talk about?">
                         </textarea>
                     </div>
-                    <button type='submit' className="py-3 px-5 font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
+                    <button type='submit' className="py-3 px-5 font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Submit</button>
                 </form>
             </div>
         </section>
